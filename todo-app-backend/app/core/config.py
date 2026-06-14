cf76836a4331c9fa1df1fcea69ee0711ad676497
mishanikin055 <mishanikin055@gmail.com>
2026-06-14 16:16:46 +0300

    Сделал docker compose, начинаю деплой на сервер

--- todo-app-backend/app/core/config.py
+++ todo-app-backend/app/core/config.py
@@ -9,9 +9,6 @@ class Settings:
     
 def get_settings() -> Settings:
     return Settings(
-        os.getenv("DATAASE_URL"),
-        cors_raw = os.getenv("CORS_ORIGINS")
-        cors_list = cors_raw.split(",")
-
-        return Settings(DATABASE_URL=db_url, cors_allowed_origin=cors_list)
+        os.getenv("DATABASE_URL"),
+        os.getenv("CORS_ORIGINS").split(",")
     )
--- todo-app-frontend/src/App.js
+++ todo-app-frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
-const API_BASE_URL = 'http://localhost:8080';
+const API_BASE_URL = '/api';
 
 function getTaskTitle(task) {
   return task.title ?? task.text ?? '';
