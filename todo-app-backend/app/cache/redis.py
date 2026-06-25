174b330311219df5dc337a9d54a77b81d19c783b
mishanikin055 <mishanikin055@gmail.com>
2026-06-25 20:09:30 +0300

    Сделал кеширование с помощью redis

--- todo-app-backend/app/cache/redis.py
+++ todo-app-backend/app/cache/redis.py
@@ -6,14 +6,16 @@ from redis import Redis
 class RedisCacheBackend:
     
     def __init__(self, redis_url: str, cache_ttl: int | None = None):
-        self.redis = Redis.from_url(redis_url, decoded_responses=True)
+        self.redis = Redis.from_url(redis_url, decode_responses=True)
         self.cache_ttl =  cache_ttl
         
     def set(self,  key: str, value: dict) -> None:
-        self.redis.set(key, json.dumps(value), ex=self.cache_ttl)
+        self.redis.set(key, json.dumps(value, default=str), ex=self.cache_ttl)
         
     def get(self, key: str) -> dict:
-        return self.redis.get(key)
+        value = self.redis.get(key)
+        if value is not None:
+            return json.loads(value)
         
     def delete(self, key: str) -> None:
         self.redis.delete(key)
--- todo-app-backend/app/services/task.py
+++ todo-app-backend/app/services/task.py
@@ -1,41 +1,52 @@
 from fastapi import HTTPException
+from fastapi.encoders import jsonable_encoder
 from sqlalchemy.orm import Session
 from app.repositories.task import TaskRepository
 from app.cache.redis import RedisCacheBackend
 from app.schemas.task import TaskSchema, TaskUpdateSchema, TaskCreateSchema
+from app.core.config import get_settings
 
 class TaskNotFound(Exception):
     """Задача не найдена"""
     ...
 
+settings = get_settings()
+
+
 class TaskService:
     def __init__(self,
-        db: Session,
-        cache_redis_url: str,
-        cache_ttl_seconds: int,
-        cache_tasks_key: str
+        db: Session
     ) -> None:
         self.db = db
         self.task_repository = TaskRepository(db)
-        self.cache = RedisCacheBackend(cache_redis_url, cache_ttl_seconds)
-        self.cache_tasks_key = cache_tasks_key
+        self.cache = RedisCacheBackend(settings.redis_url, settings.cache_ttl)
     
     def list_tasks(self) -> list[TaskSchema]:
-        cached_tasks = self.cache.get(self.cache_tasks_key)
-        if cached_tasks:
+        cached_tasks = self.cache.get(settings.cache_tasks_key)
+        if cached_tasks is not None:
             return cached_tasks
-        
+
         tasks = self.task_repository.get_all()
-        return [TaskSchema.model_validate(task) for task in tasks]
+        task_read = [TaskSchema.model_validate(task) for task in tasks]
+        tasks_for_cache = [task.model_dump() for task in task_read]
+        self.cache.set(settings.cache_tasks_key, tasks_for_cache)
+        
+        return task_read
         
     def create_task(self, task_create: TaskCreateSchema) -> TaskSchema:
+        
+        self.cache.delete(settings.cache_tasks_key)
+        
         task_orm = self.task_repository.create_task(task_create.title)
         self.db.commit()
         return TaskSchema.model_validate(task_orm)
         
         
     def update_task(self, task_id: str, task_update: TaskUpdateSchema) -> TaskSchema:
+        self.cache.delete(settings.cache_tasks_key)
+        
         task_for_update = self.task_repository.get_by_id(task_id)
+        
         if task_for_update is None:
             raise TaskNotFound("Задача не найдена")
         if task_update.completed is not None:
@@ -48,6 +59,8 @@ class TaskService:
     
     
     def delete_task(self, task_id: str) -> None:
+        self.cache.delete(settings.cache_tasks_key)
+        
         task_for_delete = self.task_repository.get_by_id(task_id)
         if task_for_delete is None:
             raise TaskNotFound("Задача не найдена")
